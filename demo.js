ad262c32197bc536f3f6f368aa676a2080017dfd
zhouying <zhy2816@163.com>
2018-10-29 19:41:16 +0800

    箭头函数的变体

---
+++ demo.js
@@ -0,0 +1,13 @@
+/**
+ * Created by Administrator on 2018/10/29.
+ */
+let fn = a => {
+    return a + 1;
+}
+console.log(fn(1));
+
+let fn2 = (a, b) => a + b
+console.log(fn2(1, 2))
+
+let fn3 = a => a + 10
+console.log(fn3(2));
